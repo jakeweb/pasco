bc328dd6bf0caff9b8a13049f21e780ef7ac8604
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-12-11 21:06:08 +0200

    improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
+var bulkSass = require('gulp-sass-bulk-import');
 var $ = require('gulp-load-plugins')();
 
 var sassPaths = [
     'bower_components/foundation-sites/scss',
-    'bower_components/motion-ui/src',
-    'scss/blocks',
-    'scss/overrides'
+    'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
     return gulp.src('scss/app.scss')
+        .pipe(bulkSass())
         .pipe($.sass({
                 includePaths: sassPaths,
                 outputStyle: 'compressed' // if css compressed **file size**
